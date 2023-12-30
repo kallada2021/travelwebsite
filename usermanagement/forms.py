ed0e355a2d603baec200c441e34670a6085fbc23
darinmilner <darin.milner@gmail.com>
2023-12-30 21:21:36 +0700

    Forms starter code added

---
+++ usermanagement/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+from django import forms 
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User 
+        fields = {"username","email","password1","password2"}
+        
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+#TODO: Complete the register view by saving the user and creating a profile and rendering the registration page
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        
+        if form.is_valid():
+            #TODO: save in DB
+            pass 
+    
+    return render(request, "registerpage goes here!!", context=context)
+        
