f91143f49e830b069d95677048da352ac744a96a
Karthikeya Allada <karthikeyaallada77@gmail.com>
2024-01-03 11:04:19 -0600

    Added login.

--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .forms import CreateUserForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import CreateUserForm, LoginForm
 from .models import UserProfile
 
 #Register view by saving the user and creating a profile and rendering the registration page
@@ -15,6 +16,7 @@ def register(request):
 
             profile = UserProfile.objects.create(user=current_user)
             profile.save()
+            return redirect("usermanagement/login.html")
 
     context = {"form":form}
     return render(request, "usermanagement/register.html", context=context)
@@ -22,3 +24,19 @@ def register(request):
 #TODO: Add a login view and a logout view
 def login(request):
     form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("index.html")
+            
+    context = {"form": form}
+    return render(request, "usermanagement/login.html", context=context)
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect("None")
